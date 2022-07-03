b588769a64289228094b0a029d0c311322a2e0c1
yuki.minami <dqyuki2003@gmail.com>
2022-07-03 08:47:49 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
     <div class='center jumbotron'>
         <div class='text-center'>
             <h1>Welcome to the Microposts</h1>
+            {{--ユーザー登録ページへのリンク--}}
+            {!! link_to_route('signup.get', 'Sing up now!', [], ['class' => 'btn btn-primary'])!!}
         </div>
     </div>
 @endsection
